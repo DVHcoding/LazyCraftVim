37554d442b10b300fb103f249a7fe98116679bad
DVHcoding <dohung2ka3@gmail.com>
2025-03-13 14:25:31 +0700

    feat: configure init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.number = true -- Bật số dòng tuyệt đối
+vim.opt.relativenumber = false -- Tắt số dòng tương đối
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8"
+vim.opt.list = false
