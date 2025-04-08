bc54df812ce7d69a50e932b0d753cc3ae685e7ca
DVHcoding <dohung2ka3@gmail.com>
2025-04-08 23:41:59 +0700

    refactor(plugins): treesitter configure

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,36 +1,53 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"astro",
-				"cmake",
-				"cpp",
-				"css",
-				"fish",
-				"gitignore",
-				"go",
-				"graphql",
-				"http",
-				"java",
-				"rust",
-				"python",
-				"php",
-				"scss",
-				"sql",
-				"svelte",
-			},
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		highlight = {
+			enable = true,
+			language_tree = true,
+			is_supported = function()
+				if vim.fn.strwidth(vim.fn.getline(".")) > 300 or vim.fn.getfsize(vim.fn.expand("%")) > 1024 * 1024 then
+					return false
+				else
+					return true
+				end
+			end,
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-
-			-- MDX
-			vim.filetype.add({
-				extension = {
-					mdx = "mdx",
-				},
-			})
-			vim.treesitter.language.register("markdown", "mdx")
-		end,
 	},
 }
+
+-- return {
+-- 	{
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 		opts = {
+-- 			ensure_installed = {
+-- 				"astro",
+-- 				"cmake",
+-- 				"cpp",
+-- 				"css",
+-- 				"fish",
+-- 				"gitignore",
+-- 				"go",
+-- 				"graphql",
+-- 				"http",
+-- 				"java",
+-- 				"rust",
+-- 				"python",
+-- 				"php",
+-- 				"scss",
+-- 				"sql",
+-- 				"svelte",
+-- 			},
+-- 		},
+-- 		config = function(_, opts)
+-- 			require("nvim-treesitter.configs").setup(opts)
+--
+-- 			-- MDX
+-- 			vim.filetype.add({
+-- 				extension = {
+-- 					mdx = "mdx",
+-- 				},
+-- 			})
+-- 			vim.treesitter.language.register("markdown", "mdx")
+-- 		end,
+-- 	},
+-- }
